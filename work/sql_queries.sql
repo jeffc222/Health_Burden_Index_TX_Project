63f9b84217b95778d442e59750c57233b641cb4c
Jeffrey <jeffcho224@gmail.com>
2025-07-20 02:18:30 -0500

    Update sql_queries.sql

--- work/sql_queries.sql
+++ work/sql_queries.sql
@@ -1,5 +1,5 @@
-/* Query 1: Build Health Burden Index */
-/* Description: Combines OBESITY, DIABETES, CSMOKING, and DEPRESSION into an average index */
+-- Query 1: Build Health Burden Index
+-- Description: Combines OBESITY, DIABETES, CSMOKING, and DEPRESSION into an average index
 
 WITH filtered_indicators AS (
   SELECT
@@ -41,3 +41,46 @@ FROM
   pivoted_data
 ORDER BY
   Health_Burden_Index DESC
+
+-- Query 2: Calculate National Average for Each Indicator
+-- Description: Used to compare individual city values to national values
+
+SELECT
+    MeasureId,
+    ROUND (AVG(Data_Value), 2) AS avg_value
+  FROM
+    places-564877.place.placelocal
+  WHERE
+    Year = 2022
+    AND MeasureId In ('OBESITY', 'DIABETES', 'CSMOKING', 'DEPRESSION')
+    AND Data_Value IS NOT NULL
+    AND Data_Value_Type = 'Crude prevalence'
+  GROUP BY
+    MeasureId
+  ORDER BY
+    MeasureId;
+
+-- Query 3: VLOOKUP Practice Query (Population Data)
+-- Description: Prepares population data to VLOOKUP in Google Sheets
+
+WITH deduped AS(
+  SELECT *,
+    ROW_NUMBER() OVER (PARTITION BY LocationID ORDER BY MeasureID) AS rn
+  FROM places-564877.place.placelocal
+  WHERE Year = 2022
+) 
+ 
+  SELECT
+    StateAbbr,
+    StateDesc,
+    LocationName,
+    LocationID,
+    TotalPopulation,
+    TotalPop18plus,
+    Geolocation,
+  FROM
+    deduped
+  WHERE
+    rn = 1
+  ORDER BY StateAbbr ASC, StateDesc ASC, LocationName ASC
+
